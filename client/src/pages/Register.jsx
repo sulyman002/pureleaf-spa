34615a0106b9b4f4c4c23894f823cc9229ffdff3
sulyman002 <oyedelesulaiman@gmail.com>
2025-10-25 01:00:40 +0100

    successfully add register page

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import background_image from "../assets/background-img.jpg";
+
+const Register = () => {
+  return (
+    <div
+      style={{ backgroundImage: `url(${background_image})` }}
+      className="w-full min-h-screen bg-cover flex items-center justify-center"
+    >
+        <div className="w-full md:w-[480px] rounded-xl px-8 bg-white py-10 shadow backdrop-blur-sm flex items-center justify-center ">
+            <form className="w-full" >
+                {/* company logo */}
+                <img src="#" alt="pureleaf-spa-logo" />
+                <h3 className="text-center font-600 text-[30px] gray-900 ">
+                    Sign Up
+                </h3>
+                {/* fills */}
+                <div className="flex flex-col gap-6 w-full">
+                    <div className="flex flex-col gap-2 ">
+                        <label htmlFor="name">
+                            Name
+                        </label>
+                        <input type="text" placeholder="Enter your name" className="rounded-lg text-[16px] py-2.5 px-3.5 border border-gray-300 " />
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default Register;
